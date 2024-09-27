13d581bb73fdc2da0616e707195d3289e8923f77
HSHwan <tnghks2866@gmail.com>
2024-09-27 14:28:44 +0900

    chore: init setup

---
+++ src/main/java/katecam/luvicookie/ditto/DittoApplication.java
@@ -0,0 +1,13 @@
+package katecam.luvicookie.ditto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DittoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/katecam/luvicookie/ditto/DittoApplicationTests.java
@@ -0,0 +1,13 @@
+package katecam.luvicookie.ditto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
