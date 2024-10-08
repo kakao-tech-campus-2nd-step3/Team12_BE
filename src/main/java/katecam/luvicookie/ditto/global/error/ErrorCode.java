64e3bae8b7bb9efb6cde379d14275ac1d55df88c
HSHwan <tnghks2866@gmail.com>
2024-10-08 15:48:34 +0900

    refactor: RequiredArgsConstructor 어노테이션 적용

--- src/main/java/katecam/luvicookie/ditto/global/error/ErrorCode.java
+++ src/main/java/katecam/luvicookie/ditto/global/error/ErrorCode.java
@@ -1,9 +1,11 @@
 package katecam.luvicookie.ditto.global.error;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
 
     STUDY_NOT_FOUND("존재하지 않는 스터디입니다.", HttpStatus.NOT_FOUND)
@@ -11,10 +13,5 @@ public enum ErrorCode {
 
     private final String message;
     private final HttpStatus httpStatus;
-
-    ErrorCode(String message, HttpStatus httpStatus) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-    }
 
 }
