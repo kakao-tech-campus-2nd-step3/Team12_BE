2b2b8eed60345b558df805105659a49c46cba19c
HSHwan <tnghks2866@gmail.com>
2024-10-15 14:55:01 +0900

    feat: 스터디 검색 기능 필터링 및 페이지네이션 고도화

--- src/main/java/katecam/luvicookie/ditto/domain/study/dao/StudyRepository.java
+++ src/main/java/katecam/luvicookie/ditto/domain/study/dao/StudyRepository.java
@@ -1,7 +1,10 @@
 package katecam.luvicookie.ditto.domain.study.dao;
 
 import katecam.luvicookie.ditto.domain.study.domain.Study;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyRepository extends JpaRepository<Study, Integer> {
+    Page<Study> findAllByTopicContainsAndNameContainsAndIsOpenOrIsOpenIsNull(String topic, String name, Boolean isOpen, Pageable pageable);
 }
