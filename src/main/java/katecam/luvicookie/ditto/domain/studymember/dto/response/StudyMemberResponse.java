70d2119abbbf5bf2a61443e0f7005ecfd111dfc1
신예준 <wns1826@gmail.com>
2024-11-05 12:45:07 +0900

    fix: datatime 표기 변경

--- src/main/java/katecam/luvicookie/ditto/domain/studymember/dto/response/StudyMemberResponse.java
+++ src/main/java/katecam/luvicookie/ditto/domain/studymember/dto/response/StudyMemberResponse.java
@@ -2,6 +2,8 @@ package katecam.luvicookie.ditto.domain.studymember.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import katecam.luvicookie.ditto.domain.member.dto.memberResponseDTO;
 import katecam.luvicookie.ditto.domain.studymember.domain.StudyMember;
 
@@ -11,10 +13,10 @@ public record StudyMemberResponse(
     @JsonProperty("role")
     String role,
     @JsonProperty("joined_at")
-    LocalDateTime joinedAt
+    String joinedAt
 ) {
 
     public StudyMemberResponse(StudyMember studyMember) {
-        this(new MemberResponse(studyMember.getMember()), studyMember.getRole().getValue(), studyMember.getJoinedAt());
+        this(new MemberResponse(studyMember.getMember()), studyMember.getRole().getValue(), studyMember.getJoinedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
     }
 }
