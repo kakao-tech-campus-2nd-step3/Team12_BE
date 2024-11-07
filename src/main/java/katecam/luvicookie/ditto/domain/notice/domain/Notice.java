b428cb78b21987bbb0c20fc53993018690285b95
HSHwan <tnghks2866@gmail.com>
2024-11-07 22:47:36 +0900

    공지 엔티티 오류 수정 (#33)

--- src/main/java/katecam/luvicookie/ditto/domain/notice/domain/Notice.java
+++ src/main/java/katecam/luvicookie/ditto/domain/notice/domain/Notice.java
@@ -2,18 +2,22 @@ package katecam.luvicookie.ditto.domain.notice.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import katecam.luvicookie.ditto.domain.member.domain.Member;
 import katecam.luvicookie.ditto.domain.notice.dto.NoticeUpdateRequest;
+import katecam.luvicookie.ditto.domain.study.domain.Study;
+import katecam.luvicookie.ditto.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -36,7 +40,6 @@ public class Notice extends BaseTimeEntity {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-
     @Builder
     public Notice(String title, String content, Study study, Member member){
         this.title = title;
@@ -51,4 +54,5 @@ public class Notice extends BaseTimeEntity {
         if(content!=null)
             this.content = noticeUpdateRequest.getContent();
     }
+
 }
