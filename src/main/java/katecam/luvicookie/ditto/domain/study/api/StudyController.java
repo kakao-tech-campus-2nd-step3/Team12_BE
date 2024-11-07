91c66e311e2bc256a5987593a793c9e03c64d7f2
HSHwan <tnghks2866@gmail.com>
2024-11-07 15:07:43 +0900

    fix: 스터디 검색 request paramter 수정

--- src/main/java/katecam/luvicookie/ditto/domain/study/api/StudyController.java
+++ src/main/java/katecam/luvicookie/ditto/domain/study/api/StudyController.java
@@ -15,11 +15,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,8 +31,8 @@ public class StudyController {
 
     @GetMapping
     public ResponseEntity<StudyListResponse> getStudyList(
-            @RequestParam @PageableDefault Pageable pageable,
-            @RequestParam @Valid StudyCriteria studyCriteria
+            @PageableDefault Pageable pageable,
+            @ModelAttribute @Valid StudyCriteria studyCriteria
     ) {
         return ResponseEntity.ok(studyService.getStudyList(pageable, studyCriteria));
     }
