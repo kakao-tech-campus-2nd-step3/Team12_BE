722f81e1f2f7561972fa7dd533cb09db1095c33a
신예준 <140493036+pnuece@users.noreply.github.com>
2024-11-08 13:19:54 +0900

    fix: ErrorCode 추가

--- src/main/java/katecam/luvicookie/ditto/global/error/ErrorCode.java
+++ src/main/java/katecam/luvicookie/ditto/global/error/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     DATE_UNABLE_TO_ATTEND("현재 출석할 수 없습니다.", HttpStatus.BAD_REQUEST),
     NOT_STUDY_LEADER("스터디장이 아닙니다.", HttpStatus.FORBIDDEN),
     NOT_STUDY_MEMBER("스터디원이 아닙니다.", HttpStatus.FORBIDDEN),
+    INVALID_TOKEN("유효하지 않은 초대 토큰입니다", HttpStatus.BAD_REQUEST),
     INVALID_ROLE("잘못된 스터디 권한입니다", HttpStatus.BAD_REQUEST)
     ;
 
