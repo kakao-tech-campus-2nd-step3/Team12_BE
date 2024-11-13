756920862854e6910b711864adf99866b51a972c
HSHwan <tnghks2866@gmail.com>
2024-11-13 23:23:25 +0900

    출석일자 테이블 코드 디폴트 수정 (#66)
    * fix: 과제 횟수 메서드 오류 수정
    
    * fix: 출석일자 삭제 오류 수정
    
    * feat: 출석일자 테이블 코드 디폴트 수정

--- src/main/java/katecam/luvicookie/ditto/domain/attendance/dto/request/AttendanceDateCreateRequest.java
+++ src/main/java/katecam/luvicookie/ditto/domain/attendance/dto/request/AttendanceDateCreateRequest.java
@@ -1,7 +1,8 @@
 package katecam.luvicookie.ditto.domain.attendance.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import  com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ public record AttendanceDateCreateRequest(
         LocalDateTime startTime,
 
         @NotNull(message = "출석 허용 시간을 입력해주세요.")
+        @Min(value = 1, message = "출석 허용 시간을 양의 정수 값으로 입력해주세요.")
         @JsonProperty("time_interval")
         Integer intervalMinutes
 ) {
