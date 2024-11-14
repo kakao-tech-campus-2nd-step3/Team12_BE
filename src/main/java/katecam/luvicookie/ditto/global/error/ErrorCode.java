1708fe051a74336b1b2fd474072d200ef51a320b
yoonsu0325 <yoonsu0325@gmail.com>
2024-11-14 10:43:45 +0900

    feat : 토큰 인증 실패시 401 에러 반환

--- src/main/java/katecam/luvicookie/ditto/global/error/ErrorCode.java
+++ src/main/java/katecam/luvicookie/ditto/global/error/ErrorCode.java
@@ -14,6 +14,9 @@ public enum ErrorCode {
     INVALID_ROLE("잘못된 스터디 권한입니다", HttpStatus.BAD_REQUEST),
     ALREADY_STUDY_MEMBER("이미 가입된 스터디입니다", HttpStatus.BAD_REQUEST),
 
+    //401 - Unauthorized
+    UNAUTHORIZED_TOKEN("유효하지 않은 액세스 토큰입니다", HttpStatus.UNAUTHORIZED),
+
     // 403 - Forbidden
     NOT_STUDY_LEADER("스터디장이 아닙니다.", HttpStatus.FORBIDDEN),
     NOT_STUDY_MEMBER("스터디원이 아닙니다.", HttpStatus.FORBIDDEN),
