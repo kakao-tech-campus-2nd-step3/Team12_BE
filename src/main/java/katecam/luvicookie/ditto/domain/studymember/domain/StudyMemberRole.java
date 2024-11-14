9a0536a71fcfe926f85fd5e294d4767cad28bc39
pnuece <dpwns1826@gmail.com>
2024-11-15 06:53:32 +0800

    feat: 스터디 권한 조회하기

--- src/main/java/katecam/luvicookie/ditto/domain/studymember/domain/StudyMemberRole.java
+++ src/main/java/katecam/luvicookie/ditto/domain/studymember/domain/StudyMemberRole.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum StudyMemberRole {
-    LEADER("스터디장"), MEMBER("스터디원"), APPLICANT("신청"), REJECTED("거절"), WITHDRAWN("탈퇴");
+    LEADER("스터디장"), MEMBER("스터디원"), APPLICANT("신청"), REJECTED("거절"), WITHDRAWN("탈퇴"), UNREGISTERED("미가입");
     private final String value;
 
     public static StudyMemberRole from(String role) {
